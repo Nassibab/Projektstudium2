3f0ec793a83be46b9fb8a6702edd72ea41208b71
Nassib <132102065+Nassibab@users.noreply.github.com>
2025-10-15 17:49:26 +0200

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-from fastapi import FastApi
+# Hello page: http://localhost:8085/hello - http://127.0.0.1:8000/hello
+# Character count: http://localhost:8085/count_chars?text=FastAPI - http://127.0.0.1:8000/counter?text=example
+# Swagger docs: http://localhost:8085/docs - http://127.0.0.1:8000/docs#/default/welcome_api_hello_get
+
+from fastapi import FastAPI, Query
+
+app= FastAPI()
+
+@app.get("/hello")
+def welcome_api():
+    return {"message": "Hello World from Nassib"}
+
+
+
+@app.get("/counter")
+def counter_api(text: str = Query(..., description="enter text")):
+    count = len(text)
+    return {"text": text, "character_count": count}
